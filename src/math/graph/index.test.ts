30883bc4bc416b8e38fc1742a74db7aebf2381c2
glyph-cat <b75ggnacj@relay.firefox.com>
2022-11-12 00:51:28 +0800

    Modifications made to graphing functions

--- src/math/graph/index.test.ts
+++ src/math/graph/index.test.ts
@@ -1,29 +1,48 @@
-import { createLinearEquation } from '.'
+import {
+  createLinearEquationFromPointAndGradient,
+  createLinearEquationFromTwoPoints,
+} from '.'
 
-describe(createLinearEquation.name, (): void => {
+describe(createLinearEquationFromPointAndGradient.name, (): void => {
 
-  // KIV: Not giving an (rationally) expected output
-  // test('temp', (): void => {
-  //   const f = createLinearEquation(20, 5, 2750, 0)
-  //   for (let x = 5; x <= 20; x++) {
-  //     const y = f(x)
-  //     console.log(`x = ${x}, y = ${y}`)
-  //   }
-  //   expect('').toBe('')
-  // })
-
-  test('2 points', (): void => {
-    const f = createLinearEquation(3, 5, 7, 9)
-    const x = 42
-    const y = f(x)
-    expect(y).toBe(44)
+  test('All positive (y = 1.5x + 0.5)', (): void => {
+    const f = createLinearEquationFromPointAndGradient(3, 5, 1.5)
+    expect(f(-10)).toBe(-14.5)
+    expect(f(42)).toBe(63.5)
   })
 
-  test('1 point, 1 gradient', (): void => {
-    const f = createLinearEquation(3, 5, 1.5)
-    const x = 42
-    const y = f(x)
-    expect(y).toBe(63.5)
+  test('Negative gradient (y = -2x + 11)', (): void => {
+    const f = createLinearEquationFromPointAndGradient(3, 5, -2)
+    expect(f(-10)).toBe(31)
+    expect(f(42)).toBe(-73)
+  })
+
+  test('One negative point (y = 6x - 25)', (): void => {
+    const f = createLinearEquationFromPointAndGradient(3, -7, 6)
+    expect(f(-10)).toBe(-85)
+    expect(f(42)).toBe(227)
+  })
+
+})
+
+describe(createLinearEquationFromTwoPoints.name, (): void => {
+
+  test('All points positive (y = 2/3x + 5)', (): void => {
+    const f = createLinearEquationFromTwoPoints(3, 7, 6, 9)
+    expect(f(-12)).toBe(-3)
+    expect(f(42)).toBe(33)
+  })
+
+  test('One point negative (y = 17/5x - 3.2)', (): void => {
+    const f = createLinearEquationFromTwoPoints(3, 7, -2, -10)
+    expect(f(-10)).toBe(-37.2)
+    expect(f(42)).toBe(139.6)
+  })
+
+  test('All points negative (y = -2x - 14)', (): void => {
+    const f = createLinearEquationFromTwoPoints(-3, -8, -4, -6)
+    expect(f(-10)).toBe(6)
+    expect(f(42)).toBe(-98)
   })
 
 })
--- src/math/graph/index.ts
+++ src/math/graph/index.ts
@@ -1,55 +1,46 @@
-import { isNumber } from '../../data/type-check'
-
 /**
- * Creates a linear equation based on a points and a gradient.
+ * Creates a linear equation based on a point and a gradient.
  * @param x1 - X coordinate of the reference point.
  * @param y1 - Y coordinate of the reference point.
  * @param m - Gradient of the equation.
  * @returns A linear equation function.
  * @example
- * const f = createLinearEquation(3, 5, 1.5)
+ * const f = createLinearEquationFromPointAndGradient(3, 5, 1.5)
  * const x = 42
  * const y = f(x)
  * console.log(y) // 63.5
  * @public
  */
-export function createLinearEquation(
+export function createLinearEquationFromPointAndGradient(
   x1: number,
-  x2: number,
-  m: number
-): (x: number) => number
+  y1: number,
+  m: number,
+): (x: number) => number {
+  const c = y1 - m * x1
+  return (x: number): number => m * x + c
+}
 
 /**
- * Creates a linear equation based on two points.
+ * Creates a linear equation from two points.
  * @param x1 - X coordinate of the first point.
  * @param y1 - Y coordinate of the first point.
  * @param x2 - X coordinate of the second point.
  * @param y2 - Y coordinate of the second point.
  * @returns A linear equation function.
  * @example
- * const f = createLinearEquation(3, 5, 7, 9)
+ * const f = createLinearEquationFromTwoPoints(3, 7, 5, 9)
  * const x = 42
  * const y = f(x)
  * console.log(y) // 44
  * @public
  */
-export function createLinearEquation(
+export function createLinearEquationFromTwoPoints(
   x1: number,
+  y1: number,
   x2: number,
-  y1: number,
-  y2: number
-): (x: number) => number
-
-/**
- * @public
- */
-export function createLinearEquation(
-  x1: number,
-  y1: number,
-  x2_or_m: number,
-  y2?: number
+  y2: number,
 ): (x: number) => number {
-  const m = isNumber(y2) ? (x2_or_m - x1) / (y2 - y1) : x2_or_m
+  const m = (y2 - y1) / (x2 - x1)
   const c = y1 - m * x1
   return (x: number): number => m * x + c
 }
