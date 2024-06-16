f0984effc708494d0bdfd77619d48c99b0d788bc
glyph-cat <b75ggnacj@relay.firefox.com>
2024-06-16 17:43:30 +0800

    Updated tests related to randomness

--- src/data/array/shuffle/index.test.ts
+++ src/data/array/shuffle/index.test.ts
@@ -1,10 +1,12 @@
 import { shuffle } from '.'
 
+// NOTE: It is possible for shuffled array to be the same as the original
+
 test(shuffle.name, (): void => {
   for (let i = 0; i < 50; i++) {
     const originalArray = [1, 2, 3, 4, 5]
     const originalSnapshot = String(originalArray)
-    const output = shuffle(originalArray)
+    const output = shuffle(originalArray, true)
     const outputSnapshot = String(output)
     if (outputSnapshot === originalSnapshot) {
       throw new Error(`[Attempt#${String(i).padStart(2, '0')}] Expected output snapshot "${outputSnapshot}" to NOT be equal to "${originalSnapshot}" but they were the same.`)
--- src/data/array/shuffle/index.ts
+++ src/data/array/shuffle/index.ts
@@ -5,23 +5,27 @@ import { getRandomNumber } from '../../../random/number'
  * @author Mike Bostock <https://bost.ocks.org/mike/shuffle>
  * @see https://bost.ocks.org/mike/shuffle (adapted)
  * @param array - The array to shuffle.
+ * @param strict - Makes sure that the shuffled array is not the same as the
+ * original one.
  * @returns A new array with the items shuffled. Position of elements in the
  * original array remains untouched.
  * @example
  * shuffle([1, 2, 3, 4, 5])
  * @public
  */
-export function shuffle<T>(array: Array<T>): Array<T> {
-  const localArray = [...array]
-  let arrayLength = localArray.length
-  // While there remain elements to shuffle…
-  while (arrayLength) {
-    // Pick a remaining element...
-    const randomIndex = getRandomNumber(0, arrayLength--)
-    // And swap it with the current element.
-    const temporaryItem = localArray[arrayLength]
-    localArray[arrayLength] = localArray[randomIndex]
-    localArray[randomIndex] = temporaryItem
-  }
-  return localArray
+export function shuffle<T>(array: Array<T>, strict = false): Array<T> {
+  const newArray = [...array]
+  do {
+    let arrayLength = newArray.length
+    // While there remain elements to shuffle…
+    while (arrayLength) {
+      // Pick a remaining element...
+      const randomIndex = getRandomNumber(0, arrayLength--)
+      // And swap it with the current element.
+      const temporaryItem = newArray[arrayLength]
+      newArray[arrayLength] = newArray[randomIndex]
+      newArray[randomIndex] = temporaryItem
+    }
+  } while (strict && String(array) === String(newArray))
+  return newArray
 }
