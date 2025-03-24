fe98fd48eac8eb4b6b6c1aeef0f7c2fed8aa9f4a
glyph-cat <sprite-charter-0d@icloud.com>
2025-03-25 00:10:17 +0800

    Updated dependencies, code cleanup

--- src/~core-ui/index.tsx
+++ src/~core-ui/index.tsx
@@ -2,13 +2,10 @@ import { KeyChordManager } from '@glyph-cat/swiss-army-knife'
 import { InputFocusTracker, LayeredFocusManager } from '@glyph-cat/swiss-army-knife-react'
 
 export const GlobalLayeredFocusManager = new LayeredFocusManager()
-
 export const GlobalInputFocusTracker = new InputFocusTracker()
-
 export const GlobalKeyChordManager = new KeyChordManager()
 
 export {
   ButtonBase as Button, FocusableView, FocusLayer, FocusRoot, Input, Select, TextArea, useCheckInputFocus, useKeyChordActivationListener,
   useKeyDownListener, useKeyUpListener, useLayeredFocusState, View
 } from '@glyph-cat/swiss-army-knife-react'
-
