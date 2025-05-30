e449c9925959a6d75c715eafe7e60c7e24129bba
glyph-cat <sprite-charter-0d@icloud.com>
2025-05-30 12:45:29 +0800

    Added new `AppUtil` methods

--- src/packages/core/src/app-utils/index.ts
+++ src/packages/core/src/app-utils/index.ts
@@ -5,10 +5,36 @@ export class AppUtils {
 
   constructor(readonly internalAppIdentifier: string) {
     this.createStorageKey = this.createStorageKey.bind(this)
+    this.clearStorage = this.clearStorage.bind(this)
+    this.clearLocalStorage = this.clearLocalStorage.bind(this)
+    this.clearSessionStorage = this.clearSessionStorage.bind(this)
+    this.clearAllStorage = this.clearAllStorage.bind(this)
   }
 
   createStorageKey(key: string): string {
     return `${this.internalAppIdentifier}/${key}`
+  }
+
+  clearStorage(storage: Storage): void {
+    for (let i = 0; i < storage.length; i++) {
+      const key = storage.key(i)
+      if (key.startsWith(this.internalAppIdentifier)) {
+        storage.removeItem(key)
+      }
+    }
+  }
+
+  clearLocalStorage(): void {
+    this.clearStorage(localStorage)
+  }
+
+  clearSessionStorage(): void {
+    this.clearStorage(sessionStorage)
+  }
+
+  clearAllStorage(): void {
+    this.clearLocalStorage()
+    this.clearSessionStorage()
   }
 
 }
