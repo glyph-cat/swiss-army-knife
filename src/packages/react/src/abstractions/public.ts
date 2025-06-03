8a514fbec523ce5e7fa5d859cd33129a11a4c210
glyph-cat <sprite-charter-0d@icloud.com>
2025-06-03 12:56:36 +0800

    New helper type: `JSXPayload`

--- src/packages/react/src/abstractions/public.ts
+++ src/packages/react/src/abstractions/public.ts
@@ -1,6 +1,11 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react'
+import { DetailedHTMLProps, HTMLAttributes, JSX } from 'react'
 
 /**
  * @public
  */
 export type GenericHTMLProps = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>
+
+/**
+ * @public
+ */
+export type JSXPayload = JSX.Element | null | false
