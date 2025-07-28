4b6c19e93a56210c8c4eefd159dd80a72e93f929
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2025-07-28 23:48:07 +0800

    Enhancements made to `VideoCamera`

--- src/packages/core/src/media/video-camera/index.ts
+++ src/packages/core/src/media/video-camera/index.ts
@@ -4,46 +4,50 @@ import { Dimension2D } from '../../math'
 import { TemplateStyles } from '../../styling'
 import { PossiblyUndefined } from '../../types'
 
-const defaultVideoConstraints: MediaStreamConstraints = {
-  video: {
-    facingMode: 'user',
-    height: { min: 240, ideal: 240 },
-    width: { min: 320, ideal: 320 },
-    // KIV: max used to be 3840x2160, there seemed to be articles in the past mentioning that the `max` value is necessary for it to work in some browsers or their versions or under certain conditions, but it doesn't make sense for us to specify a `max` value because we only want them to be as low as possible.
-  },
-}
-
-function exactDeviceIDConstraint(deviceId: string): MediaStreamConstraints {
-  return {
-    video: {
-      deviceId: {
-        exact: deviceId,
-      },
-    },
-  }
-}
-
 /**
  * @public
  */
 export class VideoCamera {
 
+  static readonly DEFAULT_CONSTRAINTS: MediaStreamConstraints = {
+    video: {
+      facingMode: 'user',
+      height: { min: 240, ideal: 240 },
+      width: { min: 320, ideal: 320 },
+      // KIV: max used to be 3840x2160, there seemed to be articles in the past mentioning that the `max` value is necessary for it to work in some browsers or their versions or under certain conditions, but it doesn't make sense for us to specify a `max` value because we only want them to be as low as possible.
+    },
+  }
+
+  static createConstraintWithExactDeviceId(
+    deviceId: string,
+    otherOptions: Omit<MediaStreamConstraints, 'video'>
+  ): MediaStreamConstraints {
+    return {
+      ...otherOptions,
+      video: {
+        deviceId: {
+          exact: deviceId,
+        },
+      },
+    }
+  }
+
   /**
    * @internal
    */
-  private mediaStream: PossiblyUndefined<MediaStream>
-
-  readonly videoElement: HTMLVideoElement
+  private M$mediaStream: PossiblyUndefined<MediaStream>
 
   /**
    * @internal
    */
-  private readonly _state = new SimpleFiniteStateManager(VideoCamera.State.CREATED, [
+  private readonly M$state = new SimpleFiniteStateManager(VideoCamera.State.CREATED, [
     [VideoCamera.State.CREATED, VideoCamera.State.STARTING],
     [VideoCamera.State.CREATED, VideoCamera.State.DISPOSED],
     [VideoCamera.State.STARTING, VideoCamera.State.STARTED],
     [VideoCamera.State.STARTING, VideoCamera.State.DENIED],
+    [VideoCamera.State.STARTING, VideoCamera.State.OVERCONSTRAINED],
     [VideoCamera.State.DENIED, VideoCamera.State.DISPOSED],
+    [VideoCamera.State.OVERCONSTRAINED, VideoCamera.State.DISPOSED],
     [VideoCamera.State.STARTED, VideoCamera.State.STOPPED],
     [VideoCamera.State.STARTED, VideoCamera.State.DENIED],
     [VideoCamera.State.STOPPED, VideoCamera.State.STARTING],
@@ -54,20 +58,22 @@ export class VideoCamera {
   })
 
   get state(): ReadOnlyStateManager<VideoCamera.State> {
-    return this._state
+    return this.M$state
   }
 
   /**
    * @internal
    */
-  private readonly _videoDimensions = new SimpleStateManager<Dimension2D>({
+  private readonly M$videoDimensions = new SimpleStateManager<Dimension2D>({
     height: 0,
     width: 0,
   })
 
   get videoDimensions(): ReadOnlyStateManager<Dimension2D> {
-    return this._videoDimensions
+    return this.M$videoDimensions
   }
+
+  readonly videoElement: HTMLVideoElement
 
   constructor() {
     this.start = this.start.bind(this)
@@ -81,39 +87,38 @@ export class VideoCamera {
   }
 
   /**
-   * @param preferredMediaDeviceId - This can be based on user preference
-   * in a persisted state.
    * @returns `true` if the operation is successful. Returns `false` if the
    * camera is starting, has already been started, or if permission is denied.
    */
-  async start(preferredMediaDeviceId?: string): Promise<boolean> {
-    if (!this._state.trySet(VideoCamera.State.STARTING)) {
+  async start(constraints: MediaStreamConstraints): Promise<boolean> {
+    if (!this.M$state.trySet(VideoCamera.State.STARTING)) {
       return false // Early exit
     }
     try {
-      const constraints = preferredMediaDeviceId
-        ? exactDeviceIDConstraint(preferredMediaDeviceId)
-        : defaultVideoConstraints
-      this.mediaStream = await navigator.mediaDevices.getUserMedia(constraints)
-      this.videoElement.srcObject = this.mediaStream
+      this.M$mediaStream = await navigator.mediaDevices.getUserMedia(constraints)
+      this.videoElement.srcObject = this.M$mediaStream
       await this.videoElement.play()
-      this._videoDimensions.set({
+      this.M$videoDimensions.set({
         height: this.videoElement.videoHeight,
         width: this.videoElement.videoWidth,
       })
       this.videoElement.height = this.videoElement.videoHeight
       this.videoElement.width = this.videoElement.videoWidth
-      // KIV: How to listen for changes in permission so that we can trigger state change to denied?
-      // we would also need to unwatch when stopped
+      // KIV: How to listen for changes in permission
+      // so that we can trigger state change from started to denied?
+      // we would also need to unwatch when stopped,
+      // suppose if it is influenced by external methods instead of using the `.stop` method
       // Not all browsers behave the same, in Firefox we can check the 'pause' event
-      return this._state.trySet(VideoCamera.State.STARTED)
+      return this.M$state.trySet(VideoCamera.State.STARTED)
     } catch (error) {
-      if (error instanceof DOMException) {
-        this._state.set(VideoCamera.State.DENIED)
-        return false
+      if (error instanceof OverconstrainedError) {
+        this.M$state.set(VideoCamera.State.OVERCONSTRAINED)
+      } else if (error instanceof DOMException) {
+        this.M$state.set(VideoCamera.State.DENIED)
       } else {
         throw error
       }
+      return false
     }
   }
 
@@ -121,14 +126,9 @@ export class VideoCamera {
    * @returns `false` if the camera is already stopped, otherwise `true`.
    */
   async stop(): Promise<boolean> {
-    await this._state.wait((s) => s !== VideoCamera.State.STARTING)
-    if (isFunction(this.mediaStream?.getTracks)) {
-      // https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamTrack/stop
-      const tracks = this.mediaStream.getTracks()
-      for (const track of tracks) { track.stop() }
-    }
-    this.videoElement.pause()
-    return this._state.trySet(VideoCamera.State.STOPPED)
+    await this.M$state.wait((s) => s !== VideoCamera.State.STARTING)
+    this.M$stopBase()
+    return this.M$state.trySet(VideoCamera.State.STOPPED)
   }
 
   // TODO: [low priority] explore this approach
@@ -142,13 +142,24 @@ export class VideoCamera {
   /**
    * @returns `false` if the instance is already disposed, otherwise `true`.
    */
-  async dispose(): Promise<boolean> {
-    await this.stop()
-    this._videoDimensions.dispose()
+  dispose(): boolean {
+    this.M$stopBase()
     this.videoElement?.remove()
-    const payload = this._state.trySet(VideoCamera.State.DISPOSED)
-    this._state.dispose()
+    this.M$videoDimensions.dispose()
+    const payload = this.M$state.trySet(VideoCamera.State.DISPOSED)
+    this.M$state.dispose()
     return payload
+  }
+
+  /**
+   * @internal
+   */
+  private M$stopBase(): void {
+    // Ref: https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamTrack/stop
+    if (isFunction(this.M$mediaStream?.getTracks)) {
+      this.M$mediaStream.getTracks().forEach((track) => track.stop())
+    }
+    this.videoElement.pause()
   }
 
 }
@@ -165,6 +176,7 @@ export namespace VideoCamera {
     CREATED,
     STARTING,
     DENIED,
+    OVERCONSTRAINED,
     STARTED,
     STOPPED,
     DISPOSED,
