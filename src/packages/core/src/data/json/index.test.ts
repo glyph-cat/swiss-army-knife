2e464ce2451a5ce99a09e802b8ef36af8e627bac
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2025-09-06 02:06:44 +0800

    Removed irrelevant tests

--- src/packages/core/src/data/json/index.test.ts
+++ src/packages/core/src/data/json/index.test.ts
@@ -1,9 +1,4 @@
-import {
-  JSONclone,
-  isJSONequal,
-  trySerializeJSON,
-} from '.'
-
+import { JSONclone, trySerializeJSON } from '.'
 
 test(JSONclone.name, () => {
   const sourceObj = { foo: 1, bar: 2 }
@@ -11,30 +6,6 @@ test(JSONclone.name, () => {
   expect(output).toStrictEqual({ foo: 1, bar: 2 })
   output.foo = 2
   expect(sourceObj).toStrictEqual({ foo: 1, bar: 2 }) // Immutability test
-})
-
-describe(isJSONequal.name, () => {
-
-  test('Is equal', () => {
-    expect(isJSONequal({ foo: 1, bar: 2 }, { foo: 1, bar: 2 })).toBe(true)
-  })
-
-  test('Not equal', () => {
-    expect(isJSONequal({ foo: 1, bar: 2 }, { baz: 1, quz: 2 })).toBe(false)
-  })
-
-  describe('Edge case: non-object types', () => {
-
-    test('Is equal', () => {
-      expect(isJSONequal(42, 42)).toBe(true)
-    })
-
-    test('Not equal', () => {
-      expect(isJSONequal(42, 41)).toBe(false)
-    })
-
-  })
-
 })
 
 describe(trySerializeJSON.name, () => {
