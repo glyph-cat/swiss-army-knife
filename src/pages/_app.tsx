b7df1a9f8182ebef7bed6806d9dc252985402dbc
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2025-11-12 16:36:36 +0800

    Code cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import {
   CheckApplePlatformProvider,
-  ClientOnly,
   CoreUIProvider,
   MaterialSymbolsOnlineLoader,
   MaterialSymbolsProvider,
@@ -13,9 +12,7 @@ import { useStateValue } from 'cotton-box-react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Fragment, JSX, StrictMode } from 'react'
-import { AppSideBar, AppSideBarContainer } from '~components/app-sidebar-wrapper'
 import { SandboxContainer } from '~components/sandbox'
-import { SandboxErrorBoundary } from '~components/sandbox/error-boundary'
 import { ENV, FixedKeyChordKey } from '~constants'
 import {
   GlobalInputFocusTracker,
