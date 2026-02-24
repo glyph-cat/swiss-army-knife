82b7ae8c820750e81d69d9596f44cd7748d80f81
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-02-25 01:12:51 +0800

    Initialized `react-ui` (2)

--- src/packages/react-ui/src/index.ts
+++ src/packages/react-ui/src/index.ts
@@ -1,1 +1,1 @@
-export default {}
+export * from './index.scripted'
---
+++ src/packages/react-ui/src/spinner/index.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+export interface ProgressRingProps {
+
+}
+
+export function ProgressRing({
+
+}: ProgressRingProps): ReactNode {
+  return <></>
+}
