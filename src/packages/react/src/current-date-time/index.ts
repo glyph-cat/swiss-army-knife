1110f3f47c888c6db22d5108bbd92fcdbb457688
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-02-26 01:49:59 +0800

    Code refactoring and cleanup

--- src/packages/react/src/current-date-time/index.ts
+++ src/packages/react/src/current-date-time/index.ts
@@ -2,21 +2,20 @@ import { isNumber } from '@glyph-cat/type-checking'
 import { useEffect, useState } from 'react'
 
 /**
- * React hook to get current {@link Date} and refreshed at regular intervals.
- * @param refreshInterval - The interval at which the current {@link Date}
+ * React hook to get current date and refreshed at regular intervals.
+ * @param refreshInterval - The interval at which the current date.
  * is refreshed.
- * @returns The current {@link Date}.
+ * @returns The current [`Date`](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Date).
  * @public
  */
 export function useCurrentDate(refreshInterval?: number): Date {
-  const [currentDate, setCurrentDate] = useState<Date>(new Date())
+  const [currentDate, setCurrentDate] = useState<Date>(() => new Date())
   useEffect(() => {
-    if (isNumber(refreshInterval)) {
-      const intervalRef = setInterval(() => {
-        setCurrentDate(new Date())
-      }, refreshInterval)
-      return () => { clearInterval(intervalRef) }
-    }
+    if (!isNumber(refreshInterval)) { return }
+    const intervalRef = setInterval(() => {
+      setCurrentDate(new Date())
+    }, refreshInterval)
+    return () => { clearInterval(intervalRef) }
   }, [refreshInterval])
   return currentDate
 }
