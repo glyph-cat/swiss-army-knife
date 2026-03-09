c0c89130090a40c38fa314c712a4e97e1aea8bc2
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-03-09 20:56:30 +0800

    Optimizations for `useDocumentFocus`

--- src/packages/react/src/document-focus/index.ts
+++ src/packages/react/src/document-focus/index.ts
@@ -1,48 +1,21 @@
-import { isNull } from '@glyph-cat/type-checking'
-import { useEffect, useRef, useState } from 'react'
-
-const THRESHOLD = 50 // ms
+import { useEffect, useState } from 'react'
 
 /**
  * @public
  */
 export function useDocumentFocus(): boolean {
-
-  const lastPointerMoveTime = useRef<number>(null)
-  // KIV: Will this work safely?
-  // if (isNull(lastPointerMoveTime.current)) {
-  //   lastPointerMoveTime.current = Date.now()
-  // }
-
-  useEffect(() => {
-    const onMouseMove = () => { lastPointerMoveTime.current = Date.now() }
-    window.addEventListener('mousemove', onMouseMove)
-    return () => { window.removeEventListener('mousemove', onMouseMove) }
-  }, [])
-
   const [isFocused, setFocusState] = useState(false)
   useEffect(() => {
-    // KIV
-    // Only start observing when document is clearly not in focus
-    // if (isFocused) { return }
-    // Probably should not use 'mousemove' alone, should consider touchscreen platforms
-    // (mouse/key/pointer?)
-    // but still, only observe for changes when document is not in focus
-    const intervalRef = setInterval(() => {
-      if (
-        isNull(lastPointerMoveTime.current) ||
-        (Date.now() - lastPointerMoveTime.current) > (THRESHOLD * 2)
-      ) {
-        setFocusState(document.hasFocus())
-      }
-    }, THRESHOLD)
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setFocusState(document.hasFocus())
+    const onFocus = () => { setFocusState(true) }
+    const onBlur = () => { setFocusState(false) }
+    window.addEventListener('focus', onFocus)
+    window.addEventListener('blur', onBlur)
     return () => {
-      if (intervalRef) {
-        clearInterval(intervalRef)
-      }
+      window.removeEventListener('focus', onFocus)
+      window.removeEventListener('blur', onBlur)
     }
-  }, [isFocused])
-
+  }, [])
   return isFocused
-
 }
