b5a048fbd59dd71da15e425984217910fcd473bf
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-03-12 00:06:45 +0800

    Added tests for `Fn`

---
+++ src/packages/foundation/src/function/index.test.ts
@@ -0,0 +1,144 @@
+import { CleanupFunction, Factory, Fn } from '.'
+
+// There will be build errors if any of these types do not behave as intended.
+
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+describe('Fn', () => {
+
+  describe('No args, no return type', () => {
+
+    test('Valid case', () => {
+      const doSomething: Fn = () => { }
+      doSomething()
+      // @ts-expect-error: Expected 0 arguments, but got 1. ts(2554)
+      doSomething(42)
+      expect('').pass('')
+    })
+
+    test('Invalid case', () => {
+      // @ts-expect-error: Target signature provides too few arguments.
+      // Expected 1 or more, but got 0. ts(2322)
+      const doSomething: Fn = (value) => { }
+      expect('').pass('')
+    })
+
+  })
+
+  describe('One arg, no return type', () => {
+
+    test('Valid case', () => {
+      const doSomething: Fn<number> = (value) => { value.toFixed(2) }
+      doSomething(42)
+      // @ts-expect-error: Expected 1 arguments, but got 0. ts(2554)
+      doSomething()
+      expect('').pass('')
+    })
+
+    test('Invalid case', () => {
+      // @ts-expect-error: Target signature provides too few arguments.
+      // Expected 2 or more, but got 1. ts(2322)
+      const doSomething: Fn<number> = (value0, value1) => { }
+      expect('').pass('')
+    })
+
+    // NOTE: `const doSomething: Fn<number> = () => { }` is still valid so this
+    // could not be tested.
+
+  })
+
+  describe('Multiple args, no return type', () => {
+
+    test('Valid case', () => {
+      const doSomething: Fn<[number, number]> = (value0, value1) => {
+        value0.toFixed(2)
+        value1.toFixed(2)
+      }
+      doSomething(42, 43)
+      // @ts-expect-error: Expected 2 arguments, but got 1.ts(2554)
+      doSomething(42)
+      expect('').pass('')
+    })
+
+    test('Invalid case', () => {
+      // @ts-expect-error: Type '(value0: any, value1: any, value2: any) => void'
+      // is not assignable to type '(args_0: number, args_1: number) => void'.
+      const doSomething: Fn<[number, number]> = (value0, value1, value2) => { }
+      expect('').pass('')
+    })
+
+  })
+
+  describe('No args, with return type', () => {
+
+    test('Valid case', () => {
+      const getSomething: Fn<void, number> = () => 42
+      const output: number = getSomething()
+      expect('').pass('')
+    })
+
+    test('Invalid case', () => {
+      // @ts-expect-error: Type 'void' is not assignable to type 'number'. ts(2322)
+      const getSomething: Fn<void, number> = () => { }
+      expect('').pass('')
+    })
+
+  })
+
+  describe('One arg, with return type', () => {
+
+    test('Valid case', () => {
+      const calcSomething: Fn<number, number> = (value) => value + 1
+      const output: number = calcSomething(42)
+      expect('').pass('')
+    })
+
+    test('Invalid case', () => {
+      // @ts-expect-error: Target signature provides too few arguments.
+      // Expected 2 or more, but got 1. ts(2322)
+      const calcSomething1: Fn<number, number> = (value0, value1) => value0 + value1
+      expect('').pass('')
+      // @ts-expect-error: Type 'void' is not assignable to type 'number'. ts(2322)
+      const calcSomething2: Fn<number, number> = (value0) => { }
+      expect('').pass('')
+    })
+
+    test('Special: Lone array argument must be wrapped in `[]`', () => {
+      const doSomething: Fn<[string[]], string> = (...values) => values.join(',')
+      expect('').pass('')
+    })
+
+  })
+
+  describe('Multiple args, with return type', () => {
+
+    test('Valid case', () => {
+      const doSomething: Fn<[number, string], string> = (num, str) => {
+        return num.toFixed(2) + str.trim()
+      }
+      expect('').pass('')
+    })
+
+    test('Invalid case', () => {
+      // @ts-expect-error: Target signature provides too few arguments.
+      // Expected 3 or more, but got 2. ts(2322)
+      const doSomething1: Fn<[number, string], string> = (num, str, x) => ''
+      // @ts-expect-error: Type 'void' is not assignable to type 'string'. ts(2322)
+      const doSomething2: Fn<[number, string], string> = (num, str) => { }
+      expect('').pass('')
+    })
+
+  })
+
+})
+
+// TODO: CleanupFunction
+// Cleanup function that accepts no parameters
+// Cleanup function that accepts one parameter
+// Cleanup function that accepts multiple parameters
+
+// TODO: Factory
+// Factory that accepts no parameters
+// Factory that accepts one parameter
+// Factory that accepts multiple parameters
--- src/packages/foundation/src/function/index.ts
+++ src/packages/foundation/src/function/index.ts
@@ -1,35 +1,70 @@
 /**
- * An alternative expression for `() => void`.
- * @public
- */
-export type EmptyFunction = () => void
-
-/**
  * A representation of a generic JavaScript function with type inference.
  * @example
  * let getSum: TypedFunction<[a: number, b: number], number>
  * getSum = (a, b) => a + b
  * @public
+ * @deprecated Please use {@link Fn|`Fn`} instead.
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type TypedFunction<Args extends any[] = any[], Payload = any> = (...args: Args) => Payload
 
 /**
+ * A representation of a generic JavaScript function with type inference.
+ * @example
+ * // Function that accepts no parameter and returns nothing
+ * const doSomething: Fn = () => { }
+ *
+ * // Function that accepts one parameter but returns nothing
+ * const setValue: Fn<number> = (value) => { someValue = value }
+ *
+ * // Function that accepts no parameter and returns a number
+ * const getRandomNumber: Fn<void, number> = () => Math.random()
+ *
+ * // Function that accepts one parameter and returns something
+ * const isEven: Fn<number, boolean> = (value) => value % 2 === 0
+ *
+ * // Function that accepts multiple parameters and returns something
+ * const getSum: Fn<[number, number], number> = (a, b) => a + b
+ *
+ * // Function that accepts one array parameter and returns something
+ * const concat: Fn<[Array<string>], string> = (values) => values.join(',')
  * @public
  */
-export type CleanupFunction = () => void
+export type Fn<T = void, K = void> = T extends unknown[]
+  ? (...args: T) => K
+  : (T extends void ? () => K : (arg: T) => K)
 
 /**
+ * An alternative expression for `() => void`.
  * @public
  */
-export type CleanupFunctionWithArgs<T extends Array<unknown>> = (...args: T) => void
+export type EmptyFunction = Fn
 
 /**
+ * @example
+ * // Cleanup function that accepts no parameters
+ * CleanupFunction // () => void
+ *
+ * // Cleanup function that accepts one parameter
+ * CleanupFunction<number> // (arg_0: number) => void
+ *
+ * // Cleanup function that accepts multiple parameters
+ * CleanupFunction<[number, string]> // (arg_0: number, arg_1: string) => void
  * @public
  */
-export type Factory<T> = () => T
+export type CleanupFunction<T = void> = Fn<T>
 
 /**
+ * @example
+ * // Factory that accepts no parameters
+ * Factory<number> // () => number
+ *
+ * // Factory that accepts one parameter
+ * Factory<number, boolean> // (arg_0: number) => boolean
+ *
+ * // Factory that accepts multiple parameters
+ * Factory<[number, string], boolean> // (arg_0: number, arg_1: string) => boolean
  * @public
  */
-export type FactoryWithArgs<T, Args extends Array<unknown>> = (...args: Args) => T
+export type Factory<T, K = void> = K extends void ? Fn<void, T> : Fn<T, K>
