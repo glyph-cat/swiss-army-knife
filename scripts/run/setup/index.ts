560a10128ccf6bb0b776f006616537e2dc71e4f1
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-03-12 20:35:47 +0800

    Updated `setup` script

--- scripts/run/setup/index.ts
+++ scripts/run/setup/index.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk'
+import { execSync } from 'child_process'
 import { existsSync, readlinkSync, rmSync, symlinkSync, unlinkSync } from 'fs'
 import path from 'path'
 import { Nullable } from '../../../src/packages/foundation/src/nullable'
@@ -8,35 +9,61 @@ import { getSiblingPackages } from '../../../tools/get-sibling-packages'
 const node_modules = 'node_modules'
 
 function main(): void {
-  console.log(`Creating symlinks for ${node_modules}...`)
-  const IGNORE_LIST = new Set([
+
+  const cwd = process.cwd()
+  const siblingPackages = getSiblingPackages()
+  const BASE_IGNORE_LIST = [
     'localization-react',
-  ])
-  getSiblingPackages().forEach((packageDirectoryName) => {
-    if (!IGNORE_LIST.has(packageDirectoryName)) {
-      linkNodeModules(process.cwd(), packageDirectoryName)
+  ]
+
+
+  console.log(`Creating symlinks for ${node_modules}...`)
+  const SYMLINKS_IGNORE_LIST = [
+    ...BASE_IGNORE_LIST,
+  ]
+  siblingPackages.forEach((packageDirectory) => {
+    if (!SYMLINKS_IGNORE_LIST.includes(packageDirectory)) {
+      linkNodeModules(cwd, packageDirectory)
     }
   })
+
+  console.log('Forwarding exports...')
+  const AFE_IGNORE_LIST = [
+    ...BASE_IGNORE_LIST,
+    'eslint-config',
+    'sleep-sort',
+  ]
+  siblingPackages.forEach((packageDirectory) => {
+    if (!AFE_IGNORE_LIST.includes(packageDirectory)) {
+      try {
+        execSync(`yarn --cwd "${path.join('.', 'src', 'packages', packageDirectory)}" afe`)
+        console.log(chalk.gray(` ✓ ${packageDirectory} · Automatically forwarded exports`))
+      } catch (e) {
+        console.log(chalk.red(` × ${packageDirectory} · Failed to forward exports`))
+      }
+    }
+  })
+
 }
 
 main()
 
-function linkNodeModules(cwd: string, packageName: string): void {
+function linkNodeModules(cwd: string, packageDirectory: string): void {
 
   const sourcePath = path.join(cwd, node_modules)
-  const targetPath = path.join(cwd, 'src', 'packages', packageName, node_modules)
+  const targetPath = path.join(cwd, 'src', 'packages', packageDirectory, node_modules)
 
   if (existsSync(targetPath)) {
     const symlinkPath = tryReadlinkSync(targetPath)
     if (isString(symlinkPath)) {
       if (symlinkPath === sourcePath) {
-        console.log(chalk.gray(` ✓ ${packageName} · Already linked`))
+        console.log(chalk.gray(` ✓ ${packageDirectory} · Already linked`))
         return // Early exit
       } else {
         try {
           unlinkSync(targetPath)
         } catch (e) {
-          console.log(chalk.red(` × ${packageName} · Failed to link, another symlink already exists but could not be removed`))
+          console.log(chalk.red(` × ${packageDirectory} · Failed to link, another symlink already exists but could not be removed`))
           console.log(e)
           return // Early exit
         }
@@ -45,7 +72,7 @@ function linkNodeModules(cwd: string, packageName: string): void {
       try {
         rmSync(targetPath, { recursive: true })
       } catch (e) {
-        console.log(chalk.red(` × ${packageName} · Failed to link, another folder already exists but could not be removed`))
+        console.log(chalk.red(` × ${packageDirectory} · Failed to link, another folder already exists but could not be removed`))
         console.log(e)
         return // Early exit
       }
@@ -54,9 +81,9 @@ function linkNodeModules(cwd: string, packageName: string): void {
 
   try {
     symlinkSync(sourcePath, targetPath)
-    console.log(chalk.green(` + ${packageName} · Link created`))
+    console.log(chalk.green(` + ${packageDirectory} · Link created`))
   } catch (e) {
-    console.log(chalk.red(` × ${packageName} · Failed to create link`))
+    console.log(chalk.red(` × ${packageDirectory} · Failed to create link`))
     console.error(e)
   }
 
