202dd8d6f53bc630117db3fdde65cdf77d931c43
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-08-19 04:55:00 +0800

    updated configs

--- packages/core/jest.post-env-setup.ts
+++ packages/core/jest.post-env-setup.ts
@@ -2,5 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import '@glyph-cat/custom-tools/jest-setup-console-spy'
 import '@testing-library/jest-dom'
-import '../../../tools/jest-setup-console-spy'
--- packages/core/src/data/array/interleave/index.test.ts
+++ packages/core/src/data/array/interleave/index.test.ts
@@ -2,5 +2,5 @@ import { interleave } from '.'
 
 test(interleave.name, (): void => {
   const output = interleave(['a', 'b', 'c'], ['d', 'e', 'f'], ['g'])
-  expect(output).toStrictEqual(['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+  expect(output).toStrictEqual(['a', 'd', 'g', 'b', 'e', 'c', 'f'])
 })
