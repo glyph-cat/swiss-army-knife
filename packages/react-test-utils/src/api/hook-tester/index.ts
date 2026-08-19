9de6f5da1675016be5bddd054ca694478ac4eac4
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-08-20 02:11:28 +0800

    fixed misconfigurations in react-test-utils

--- packages/react-test-utils/src/api/hook-tester/index.ts
+++ packages/react-test-utils/src/api/hook-tester/index.ts
@@ -1,5 +1,4 @@
 import { renderHook } from '@testing-library/react'
-import { createElement, PropsWithChildren, Suspense, useEffect } from 'react'
 
 export interface CustomRenderResultMetadata {
   renderCount: number
@@ -30,49 +29,6 @@ export function customRenderHook<Result, Props>(
     forceUpdate: () => {
       hook.rerender(lastProps)
     },
-  }
-
-}
-
-export interface CustomSuspenseTesterResultMetadata extends CustomRenderResultMetadata {
-  isSuspended: boolean
-}
-
-export interface CustomSuspenseTesterResult<Result, Props> extends Omit<CustomRenderHookResult<Result, Props>, 'getMetadata'> {
-  getMetadata(): CustomSuspenseTesterResultMetadata
-}
-
-export function renderSuspenseTester<Result, Props>(
-  ...args: Parameters<typeof customRenderHook<Result, Props>>
-): CustomSuspenseTesterResult<Result, Props> {
-
-  const metadata = { isSuspended: false }
-
-  const FallbackComponent = (): undefined => {
-    useEffect(() => {
-      metadata.isSuspended = true
-      return () => { metadata.isSuspended = false }
-    }, [])
-  }
-
-  const wrapper = ({ children }: PropsWithChildren) => (
-    createElement(Suspense, {
-      fallback: createElement(FallbackComponent),
-    }, children)
-  )
-
-  const [callback, options, ...remainingArgs] = args
-  const hook = customRenderHook<Result, Props>(callback, {
-    ...options,
-    wrapper,
-  }, ...remainingArgs)
-
-  return {
-    ...hook,
-    getMetadata: () => ({
-      ...hook.getMetadata(),
-      ...metadata,
-    }),
   }
 
 }
