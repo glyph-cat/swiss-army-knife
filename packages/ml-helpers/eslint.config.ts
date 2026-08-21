03089e8b9bfdcc7077088216c7b464c84e56aa8e
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-08-22 05:51:16 +0800

    updated configs

---
+++ packages/ml-helpers/eslint.config.ts
@@ -0,0 +1,37 @@
+import { recommended as baseRecommended } from '@glyph-cat/eslint-config/base'
+import { recommended as jestRecommended } from '@glyph-cat/eslint-config/jest'
+import { Severity } from '@glyph-cat/eslint-config/src'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+
+export default defineConfig(
+  baseRecommended,
+  jestRecommended,
+  {
+    rules: {
+      'no-console': Severity.OFF, // temp
+      '@typescript-eslint/no-namespace': Severity.OFF,
+      '@typescript-eslint/no-require-imports': Severity.OFF,
+      '@typescript-eslint/no-empty-object-type': Severity.WARN, // temp
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: [
+      './babel.config.js',
+      './config/rollup.config.js',
+    ],
+  },
+)
